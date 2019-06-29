8397de6ff96dc9ce944b78a846cc192d781606c2
kaylee.bae <kaylee.bae@within.co.kr>
2019-06-29 19:54:22 +0900

    대출 및 적금 상품 추천

--- kb-server/routes/index.js
+++ kb-server/routes/index.js
@@ -110,4 +110,49 @@ router.get('/get/mbti_desc', function(req, res, next) {
 
 });
 
+router.get('/get/product', function(req, res, next) {
+  var uno = req.query.uno;
+
+  var order_fields = "prod_id,prod_category,prod_name,favor_job_category,price_str,url".split(",");
+  var order_methods = ["desc", "asc"];
+
+  var order_field_idx = Math.floor(Math.random() * order_fields.length);
+  var order_field = order_fields[order_field_idx];
+
+  var order_method_idx = Math.floor(Math.random() * order_methods.length);
+  var order_method = order_methods[order_method_idx];
+
+  var stmt = `SELECT pd.* FROM product pd ` + 
+    `INNER JOIN person_info pi ` + 
+    `ON pi.job_category = pd.favor_job_category ` + 
+    `WHERE pi.uno = ${uno} ` + 
+    `ORDER BY ${order_field} ${order_method}`;
+
+  connection.query(stmt, function(err, result) {
+
+    var resList = [];
+
+    var rentIdx = 2;
+    var saveIdx = 1;
+
+    for( idx in result ) {
+      var prod = result[idx];
+      if(prod.prod_category == 0) { //대출일경우 2개 추천
+        if(rentIdx > 0) {
+          resList.push(prod);
+          rentIdx -=1;
+        }
+      } else {
+        if(saveIdx > 0) {
+          resList.push(prod);
+          saveIdx -=1;
+        }
+      }
+    }
+
+    res.send(resList);
+  });
+
+});
+
 module.exports = router;
