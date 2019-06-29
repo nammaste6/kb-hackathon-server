235c0408288bef4d5aa0b7a573907e607ee77476
kaylee.bae <kaylee.bae@within.co.kr>
2019-06-29 18:07:33 +0900

    spark shell script add

--- kb-server/test.js
+++ kb-server/test.js
@@ -1,26 +1,9 @@
-var test = "1234k"
-var str = `"${test}" is ES6` + ` asdf`
+var exec = require("child_process").exec;
 
-var uno = 1;
+var jar_file = '/Users/kaylee/jsproj/kb-hackathon-server/generator/target/scala-2.11/generator_2.11-0.1.jar';
+var spark_submit_cmd = `spark-submit --deploy-mode cluster --master yarn --deploy-mode cluster --driver-memory 4g --executor-memory 2g --executor-cores 2 --class ImportActlogToS3 ${jar_file}`
 
-var stmt2 = `SELECT` +
-    `  *, ` +
-    `  (w_rate * 10) as percent` +
-    `FROM PLACE p` +
-    `INNER JOIN (` +
-    `  SELECT pi.*, w_rate` +
-    `  FROM person_info pi` +
-    `  INNER JOIN (` +
-    `    SELECT mm.o_mbti, IF(count(mm.o_mbti) <= 1, avg(mm.rate), ((count(mm.o_mbti) / 10) + 1) * avg(mm.rate)) AS w_rate` +
-    `    FROM mbti_ml mm` +
-    `    INNER JOIN person_info pi` +
-    `      ON (pi.u_mbti = mm.s_mbti or pi.pay_mbti = mm.s_mbti) AND pi.uno = ${uno}` +
-    `    GROUP BY mm.o_mbti` +
-    `  ) AS recm` +
-    `  ON pi.pay_mbti = recm.o_mbti` +
-    `  WHERE pi.uno != ${uno}` +
-    `    AND pi.is_lessor = 0` +
-    `) AS rep` +
-    `ON p.uno = rep.uno`;
-
-console.log(stmt2);
+console.log(spark_submit_cmd);
+exec("ls -al /Users/kaylee/jsproj/kb-hackathon-server/kb-server", function(err, stdout, stderr) {
+  console.log(stdout);
+});
